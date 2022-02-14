a25e757fd655cbbd4e472b81f8d26c3309f8018e
Makinfantasy <beingp0z1t1v3@gmail.com>
2022-02-14 20:54:45 +0300

    Необоходимое сохранение для переноса данных

--- tests/secretar_test.py
+++ tests/secretar_test.py
@@ -1,10 +1,19 @@
+import builtins
 import unittest
-from unittest.mock import patch
-from app import get_doc_shelf, get_doc_owner_name, get_all_doc_owners_names, remove_doc_from_shelf, add_new_doc, documents
+from unittest import mock
+from unittest.mock import patch, Mock
+
+import app
+from app import get_doc_shelf, get_doc_owner_name, get_all_doc_owners_names, remove_doc_from_shelf, add_new_doc, documents, directories
 
 
 class TestDocFunctions(unittest.TestCase):
-    def test_add_doc(self):
+    def test_add_doc_normal_flow(self):
+        # Patch dictionary in target scope
+        with patch.dict(directories, {}, clear=True), \
+                patch.object(app, documents, []), \
+                patch('builtins.input', side_effect=['1', 'passport', 'Test Owner', '1']):
+            add_new_doc()
         pass
 
     def test_get_shelf(self):
